2e60b082486fc103e4f058200b2b5abc3c9f9e0a
LauraGoedkoop <lauragoedkoop@msn.com>
2020-02-27 16:28:57 +0100

    opdracht 1

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -1,4 +1,6 @@
-
+/*/jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
 
 var heart = document.querySelector('.heart');
 
