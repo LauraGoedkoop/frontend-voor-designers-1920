bbd8cc6e4921b821b35bcda9f5c8a1dd077c549a
LauraGoedkoop <lauragoedkoop@msn.com>
2020-04-01 15:39:32 +0200

    opdracht 3

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -43,78 +43,81 @@ request.onload = function () {
 //we noemen de parameter jsonObj om ons zelf eraan te herinneren dat het javascript object afkomstig is van JSON 
 
 function showCover(movies) {
-  var myH1 = document.createElement('h1');
-  myH1.textContent = movies[3].title;
-  myH1.setAttribute("class", "boe");
-  header.appendChild(myH1);
-  
-  
-  for (var i = 0; i < movies.length; i++){
-   
-  var section = document.createElement('section');
-  section.setAttribute('tabindex', 0);
+//  var myH1 = document.createElement('h1');
+//  myH1.textContent = movies[3].title;
+//  myH1.setAttribute("class", "boe");
+//  header.appendChild(myH1);
 
-  var cover = document.createElement('img');
-  cover.setAttribute("src", movies[i].cover);
-  section.appendChild(cover);
-  
-  var article = document.createElement('article');
-  var myTitle = document.createElement('h2');
-  article.appendChild(myTitle);
-  section.appendChild(article);
-  myTitle.textContent = movies[i].title;
+
+  for (var i = 0; i < movies.length; i++) {
+
+    var section = document.createElement('section');
+    section.setAttribute('tabindex', 0);
+
+    var cover = document.createElement('img');
+    cover.setAttribute("src", movies[i].cover);
+    section.appendChild(cover);
+
+    var article = document.createElement('article');
+    var myTitle = document.createElement('h2');
+    article.appendChild(myTitle);
+    section.appendChild(article);
+    myTitle.textContent = movies[i].title;
+
+    var release_date = document.createElement('h4');
+    release_date.textContent = movies[i].release_date;
+    article.appendChild(release_date);
+
+    var genres = document.createElement('h3');
+    genres.textContent = movies[i].genres;
+    article.appendChild(genres);
+
+    var simple_plot = document.createElement('p');
+    simple_plot.textContent = movies[i].simple_plot;
+    article.appendChild(simple_plot);
     
-  var genres = document.createElement('h3');
-  genres.textContent = movies[i].genres;
-  article.appendChild(genres);  
-    
-//  var reviews = document.createElement('p');  
-//  reviews.textContent = reviews[i].score;
-//  article.appendChild(reviews); 
-//  console.log(reviews.score);  
-  
-  var plot = document.createElement('p');
-  plot.textContent = movies[i].plot;
-  article.appendChild(plot);
-    
-  div.appendChild(section);
+    var trailer = document.createElement('iframe');
+    trailer.setAttribute("src", movies[i].trailer);
+    article.appendChild(trailer);
+
+    div.appendChild(section);
 
   }
-  
-  
-//
-//  var cover = document.createElement('img');
-//  cover.setAttribute("src", movies[1].cover);
-//  foto2.appendChild(cover);
-//
-//
-//  var cover = document.createElement('img');
-//  cover.setAttribute("src", movies[2].cover);
-//  foto3.appendChild(cover);
-//
-//
-//  var cover = document.createElement('img');
-//  cover.setAttribute("src", movies[3].cover);
-//  foto4.appendChild(cover);
-//
-//
-//  var cover = document.createElement('img');
-//  cover.setAttribute("src", movies[4].cover);
-//  foto5.appendChild(cover);
-//
-//  var cover = document.createElement('img');
-//  //  cover.src = movies[3].cover;
-//  cover.setAttribute("src", movies[5].cover);
-//  //  cover.id="dljnljedn";
-//  foto6.appendChild(cover);
-//
-//  var cover = document.createElement('img');
-//  cover.setAttribute("src", movies[0].cover);
-//  foto7.appendChild(cover);
-//
-//  var cover = document.createElement('img');
-//  cover.setAttribute("src", movies[1].cover);
-//  foto8.appendChild(cover);
-//
+
+
+  //
+  //  var cover = document.createElement('img');
+  //  cover.setAttribute("src", movies[1].cover);
+  //  foto2.appendChild(cover);
+  //
+  //
+  //  var cover = document.createElement('img');
+  //  cover.setAttribute("src", movies[2].cover);
+  //  foto3.appendChild(cover);
+  //
+  //
+  //  var cover = document.createElement('img');
+  //  cover.setAttribute("src", movies[3].cover);
+  //  foto4.appendChild(cover);
+  //
+  //
+  //  var cover = document.createElement('img');
+  //  cover.setAttribute("src", movies[4].cover);
+  //  foto5.appendChild(cover);
+  //
+  //  var cover = document.createElement('img');
+  //  //  cover.src = movies[3].cover;
+  //  cover.setAttribute("src", movies[5].cover);
+  //  //  cover.id="dljnljedn";
+  //  foto6.appendChild(cover);
+  //
+  //  var cover = document.createElement('img');
+  //  cover.setAttribute("src", movies[0].cover);
+  //  foto7.appendChild(cover);
+  //
+  //  var cover = document.createElement('img');
+  //  cover.setAttribute("src", movies[1].cover);
+  //  foto8.appendChild(cover);
+  //
 
 }
